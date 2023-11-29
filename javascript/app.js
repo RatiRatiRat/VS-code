7bff44706f0bdd6ce5bb7f10e1de76803f0577a3
RatiRatiRat <150704351+RatiRatiRat@users.noreply.github.com>
2023-11-29 19:44:18 +0300

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,23 @@
+const players = [
+
+     {
+
+     
+          firstname:'Cristiano'
+          lastname:'Ronaldo'
+          height,:'1.87'
+          age:'38'
+          current team:'al nassr'
+          networth:'500mil'
+          salary:'70mil'
+          nationality:'Portuguese'
+          goals scored:'850'
+
+
+
+      }
+
+
+];
+
+ console.log(players)
